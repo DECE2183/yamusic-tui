16b77deb542312203bfe81b59018ee03e282ecdf
Vladimir Korshunov <korshunovwow.github@gmail.com>
2026-03-19 19:17:40 +0300

    Added volume indicator to the player progress bar.

--- ui/components/tracker/tracker.go
+++ ui/components/tracker/tracker.go
@@ -46,7 +46,8 @@ func (p ProgressControl) Value() float64 {
 }
 
 const (
-	_VOLUME_FADE_STEPS = 2
+	_VOLUME_FADE_STEPS      = 2
+	_VOLUME_INDICATOR_WIDTH = 5 // " NNN%"
 )
 
 var rewindAmount = time.Duration(config.Current.RewindDuration) * time.Second
@@ -130,8 +131,11 @@ func (m *Model) View() string {
 		playButton = style.ActiveButtonStyle.Padding(0, 1).Margin(0).Render(style.IconStop)
 	}
 
+	volumeText := fmt.Sprintf(" %3d%%", int(math.Round(m.volume*100)))
+	volumeIndicator := style.TrackVersionStyle.Render(volumeText)
+
 	tracker := style.TrackProgressStyle.Render(m.progress.View())
-	tracker = lipgloss.JoinHorizontal(lipgloss.Top, playButton, tracker)
+	tracker = lipgloss.JoinHorizontal(lipgloss.Top, playButton, tracker, volumeIndicator)
 
 	if m.showLyrics {
 		tracker = lipgloss.JoinVertical(lipgloss.Left, m.renderLyrics(), "", tracker)
@@ -299,7 +303,7 @@ func (m *Model) Update(message tea.Msg) (*Model, tea.Cmd) {
 
 func (m *Model) SetWidth(width int) {
 	m.width = width
-	m.progress.Width = width - 9
+	m.progress.Width = width - 9 - _VOLUME_INDICATOR_WIDTH
 	m.help.Width = width - 4
 }
 
